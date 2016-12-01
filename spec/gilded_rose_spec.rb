094af454fd83852bdaaf7862eb1fb4399054e987
Frankie Bell <fbell.pro@gmail.com>
2016-12-01 12:33:39 +0000

    added initial tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,45 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
-
 describe GildedRose do
+
+  before(:each) do
+    @item1 = [Item.new('Aged Brie', 20, 15)]
+    @rose1 = GildedRose.new(@item1)
+    @item2 = [Item.new('Something tasty', 15, 20)]
+    @rose2 = GildedRose.new(@item2)
+    @item3 = [Item.new('Sulfuras, Hand of Ragnaros', 5, 10)]
+    @rose3 = GildedRose.new(@item3)
+  end
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      @rose1.update_quality()
+      expect(@item1[0].name).to eq "Aged Brie"
+      @rose2.update_quality()
+      expect(@item2[0].name).to eq "Something tasty"
     end
+
+    it 'changes the quality after each update' do
+      @rose1.update_quality()
+      expect(@item1[0].quality).to eq 16
+    end
+
+    it 'decreases the sell in number after each update' do
+      @rose2.update_quality()
+      expect(@item2[0].sell_in).to eq 14
+    end
+
+    it 'cannot decrease quality futher than 0' do
+      21.times{@rose2.update_quality()}
+      expect(@item2[0].quality).to eq 0
+    end
+
+    context 'Sulfuras tests' do
+      it 'does not change the sell in number after update' do
+        @rose3.update_quality()
+        expect(@item3[0].sell_in).to eq 5
+      end
+    end
+
   end
+
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
-require 'rspec'
+require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+
+end
