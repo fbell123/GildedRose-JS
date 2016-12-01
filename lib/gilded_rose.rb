4ea242979c69856d9ecf422636a43a2e93d90064
Frankie Bell <fbell.pro@gmail.com>
2016-12-01 16:49:04 +0000

    methods for each item added

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -54,3 +54,57 @@ class GildedRose
     end
   end
 end
+
+def sulfuras_item
+  item.name = "Sulfuras, Hand of Ragnaros"
+  item.quality = item.quality
+  item.sell_in = item.sell_in
+end
+
+def aged_brie_item
+  item.name = "Aged Brie"
+  if item.quality < 50 && item.sell_in > 0
+    item.quality += 1
+    item.sell_in -= 1
+  elsif item.sell_in < 0
+    item.quality -= 2
+    item.sell_in -= 1
+  end
+end
+
+def backstage_item
+  item.name = "Backstage passes to a TAFKAL80ETC concert"
+  if item.quality < 50 && item.sell_in > 10
+    item.quality += 1
+    item.sell_in -= 1
+  elsif item.quality < 50 && item.sell_in < 10 && item.sell_in > 6
+    item.quality += 2
+    item.sell_in -= 1
+  elsif item.quality < 50 && item.sell_in < 6 && item.sell_in > 0
+    item.quality += 3
+    item.sell_in -= 1
+  elsif item.sell_in < 0
+    item.quality -= 2
+    item.sell_in -= 1
+  end
+end
+
+def conjured_item
+  item.name = "Conjured"
+  if item.quality < 50 && item.sell_in > 0
+    item.quality += 1
+    item.sell_in -= 2
+  elsif item.sell_in < 0
+    item.quality -= 2
+    item.sell_in -= 1
+  end
+end
+
+
+def item_quality
+  if item.name = "Aged Brie" && item.quality < 50
+    item.quality = item.quality + 1
+  elsif item.name != "Aged Brie" && item.name != "Sulfuras, Hand of Ragnaros"
+    item.quality = item.quality - 1
+  end
+end
